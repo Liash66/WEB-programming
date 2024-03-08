bdf99cee2b713dc8ae223b4f0f8777f728ecc580
Liash66 <AKoshelev2004@yandex.ru>
2024-03-08 15:38:10 +0200

    done task 2

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -9,3 +9,17 @@ $order .= " and a large soda";
 
 //Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name";
+
+
+$a = 220;
+echo "\nVariable 1: $a";
+
+$b = 6.9;
+echo "\nVariable 1: $b";
+
+echo "12";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$difference = $last_month - $this_month;
+echo "\nDifference: $difference";
