e1fcee8f4b890488befb7c847b74e156fffba417
Liash66 <AKoshelev2004@yandex.ru>
2024-03-23 14:09:32 +0200

    done task_1_b

--- lab_3/code/index.php
+++ lab_3/code/index.php
@@ -3,3 +3,12 @@
 $str = 'ahb acb aeb aeeb adcb axeb';
 preg_match_all('/a..b/', $str, $matches);
 print_r($matches[0]);
+
+$str = 'a1b2c3';
+$new_str = preg_replace_callback('/\d+/', function($matches)
+{
+    $num = $matches[0];
+    return $num * $num * $num;
+}, $str);
+
+echo "\n", $new_str;
