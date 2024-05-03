8519cd9745de43f00bb521b5fcf0db9bfbc5d828
Liash66 <AKoshelev2004@yandex.ru>
2024-05-03 19:54:50 +0200

    done composer install

---
+++ lab_4/code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'web/lab_4',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e7e84ca05601116ed07dba8943a2b1660b7f95c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'web/lab_4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e7e84ca05601116ed07dba8943a2b1660b7f95c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
